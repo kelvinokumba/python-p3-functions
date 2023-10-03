f1590905a148f0d666f37bf6d3d1340962dfa4d2
Kelvin okumba <okumbakelvin@gmail.com>
2023-10-03 19:46:29 +0300

    completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
-
 def greet_programmer():
-    pass
+    print(f"Hello, programmer!")
+    
+greet_programmer()   
 
-def greet(name):
-    pass
+def greet(name='programmer'):
+    print(f'Hell0', {name})
+
+greet("james")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'Hello, {name}!')
+
+greet_with_default()
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
 
 def halve(number):
-    pass
+    return number/2
